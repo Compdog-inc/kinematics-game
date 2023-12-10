357a6d56b4edb91960f89f7b11477baa86ba5a57
Compdog Inc <compdog.inc@gmail.com>
2023-12-10 13:01:58 -0500

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import { CssVarsProvider } from '@mui/joy/styles';
 import CssBaseline from '@mui/joy/CssBaseline';
+import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }: AppProps) {
     return (
@@ -8,6 +9,7 @@ function App({ Component, pageProps }: AppProps) {
             <CssVarsProvider>
                 <CssBaseline />
                 <Component {...pageProps} />
+                <Analytics />
             </CssVarsProvider>
         </div>
     )
