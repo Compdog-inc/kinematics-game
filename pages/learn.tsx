f19fc66a2f5afad56e250caecb212c3d9c086d5d
Compdog Inc <compdog.inc@gmail.com>
2024-01-20 22:46:43 -0500

    Add learn page stub

---
+++ pages/learn.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Learn() {
+    return (
+        <>
+            <Head>
+                <title>Pixel Perfect Kinematics | Lessons</title>
+            </Head>
+        </>
+    )
+}
