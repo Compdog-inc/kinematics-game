54aad37d773f02bd017feae02db6801173048c93
Compdog Inc <compdog.inc@gmail.com>
2024-01-20 21:23:24 -0500

    Create welcome page stub

---
+++ pages/welcome.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Welcome() {
+    return (
+        <>
+            <Head>
+                <title>Pixel Perfect Kinematics | Welcome</title>
+            </Head>
+        </>
+    )
+}
