23f571fe104460b921370feda3913749e056de62
Compdog Inc <compdog.inc@gmail.com>
2024-01-28 19:02:39 -0500

    Add puzzles page stub

---
+++ pages/puzzles.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Puzzles() {
+    return (
+        <>
+            <Head>
+                <title>Pixel Perfect Kinematics | Puzzles</title>
+            </Head>
+
+        </>
+    )
+}
